6939b7d054002da47a40ac367b1e1f954a9e432b
LAI <1434234588@qq.com>
2019-09-24 19:58:19 +0800

    people body

---
+++ Body.java
@@ -0,0 +1,29 @@
+public class Body{
+	public static void hands(){
+		int i = 2;
+		System.out.println("i have" + i +"hands");
+	}
+	public static void main(String[] args){
+		int i = 10;
+		hands();
+		System.out.println("i have" + i + "fingers");
+	}
+}
+//body.java
+/*
+public class Body{
+	public void hands(){
+		int i = 2;
+		System.out.println("i have" + i +"hands");
+	}
+	public static void st(Body c){
+		c.hands();
+	}
+	public static void main(String[] args){
+		Body stoke = new Body();
+		st(stoke);
+		int i = 10;
+		System.out.println("i have" + i + "fingers");
+	}
+}
+*/
