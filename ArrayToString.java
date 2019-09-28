2fa5cd86d04f26aa0ab0cb25064c07e2c4dbb866
LAI <1434234588@qq.com>
2019-09-28 12:35:00 +0800

    打印数组

---
+++ ArrayToString.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class ArrayToString{
+	public static void main(String[] args){
+		int a[] = {1,3,5,2,9};
+		String aname = Arrays.toString(a);//String前面的a代表数组,后面也是
+		System.out.println(aname);
+	}
+}
