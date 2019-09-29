4c2e5633fa70861b54d70cb960a625bf0166fa27
LAI <1434234588@qq.com>
2019-09-29 21:47:22 +0800

    数组的填充

---
+++ ArrayFill.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class ArrayFill{
+	public static void main(String[] args){
+		int[] a = new int[3];
+		Arrays.fill(a, 3);
+		System.out.println(Arrays.toString(a));
+	}
+}
