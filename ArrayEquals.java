1df38bd1ccbfc4888d2863aadbcf71ea09b02138
LAI <1434234588@qq.com>
2019-09-29 21:50:56 +0800

    判断数组是否相等

---
+++ ArrayEquals.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class ArrayEquals{
+	public static void main(String[] args){
+		int a[] = {1,3,4};
+		int b[] = {2,4,8,9};
+		boolean answer = Arrays.equals(a,b);
+ 		System.out.println(answer);
+	}
+}
